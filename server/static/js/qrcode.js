9e7c458416b33e179608d3746fb82832f0d1dee4
Raye <okps123@naver.com>
2018-06-29 22:51:35 +0900

    Add pill data in qrcode

---
+++ server/static/js/qrcode.js
@@ -0,0 +1,48 @@
+(function($){
+    $(function(){
+        $('#save_pill').on('click', function() {
+            var modal = $("#modal1");
+
+            var pill_name = modal.find("#pill_name").val();
+            if(pill_name === "") {
+                alert("처방 의약품 명칭을 입력하세요.")
+                return;
+            }
+            alert(pill_name);
+
+            var one_dose = modal.find("#one_dose").val();
+            if(one_dose === "") {
+                alert("1회 투여량을 입력하세요.")
+                return;
+            }
+            alert(one_dose);
+            
+            var day_dose = modal.find("#day_dose").val();
+            if(day_dose === "") {
+                alert("1일 투여횟수를 입력하세요.")
+                return;
+            }
+            
+            var dose_days = modal.find("#dose_days").val();
+            if(dose_days === "") {
+                alert("투약 일수를 입력하세요.")
+                return;
+            }
+            
+            var dose_time = modal.find("#dose_time").val();
+
+            // 새 리스트 생성
+            var pill_item = $("#pill-template").clone();
+            pill_item.show();
+            pill_item.find("#pill_name").text(pill_name);
+            pill_item.find("#one_dose").text(one_dose);
+            pill_item.find("#day_dose").text(day_dose);
+            pill_item.find("#dose_days").text(dose_days);
+            pill_item.find("#dose_time").text(dose_time);
+
+            // 추가
+            $(".collection").append(pill_item);
+        });
+    });
+  })(jQuery);
+  
