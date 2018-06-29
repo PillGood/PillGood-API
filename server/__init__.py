478b82f84d230b0eef40589bbe7a9f16bd8ae940
Raye <okps123@naver.com>
2018-06-29 11:06:38 +0900

    Setting use ascii

--- server/__init__.py
+++ server/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['JSON_AS_ASCII'] = False
 
 from server.routes.pill import pill_bp
 app.register_blueprint(pill_bp)
